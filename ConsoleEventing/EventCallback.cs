0d2e4c5bbd1cbdde30914c8d03849d192246efa7
Johann Mortara <johann.mortara@etu.unice.fr>
2018-04-05 10:51:04 +0200

    working service

---
+++ ConsoleEventing/EventCallback.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ConsoleEventing.EventService;
+
+namespace ConsoleEventing
+{
+    public class EventCallback : EventService.IVelibServiceCallback
+    {
+        public void StationChanged(Station station)
+        {
+            Console.WriteLine($"LOL station changed {station.name}");
+        }
+    }
+}
---
+++ ConsoleEventing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEventing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instanceContext = new InstanceContext(new EventCallback());
+            var velibServiceClient = new EventService.VelibServiceClient(instanceContext);
+            velibServiceClient.SubscribeStationChange("Toulouse");
+            Console.ReadLine();
+        }
+    }
+}
